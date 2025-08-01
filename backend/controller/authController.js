664450a11454c910fd8326a993a1e4073d6bb159
Dev-pratap-s <devpratapsinghchauhan523@gmail.com>
2025-08-01 14:57:57 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -43,7 +43,7 @@ export const Registration = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
@@ -78,8 +78,8 @@ export const login = async (req, res) => {
         // ✅ Set cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false, // 🔁 set true in production
-            sameSite: "strict",
+            secure: true, // 🔁 set true in production
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
@@ -126,8 +126,8 @@ export const googleLogin = async (req,res)=>{
         // ✅ Set cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false, // 🔁 set true in production
-            sameSite: "strict",
+            secure: true, // 🔁 set true in production
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
@@ -151,8 +151,8 @@ export const adminLogin = async (req, res) => {
 
             res.cookie("token", token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: "Strict",
+                secure: true,
+                sameSite: "none",
                 maxAge: 1 * 24 * 60 * 60 * 1000
             });
 
